22485c4030137520e2424e631f8986face48b0ab
Легостин Иван <ivan.legostin@gazprombank.ru>
2025-01-12 19:45:18 +0300

    Basic functional

---
+++ index.js
@@ -0,0 +1,89 @@
+// to erase the board and remember last user density choice
+let lastDensity = "small"
+
+// remember when mouse is up/down
+let mouseDown = false
+
+buildGridByDensity(lastDensity)
+
+const gridElement = document.querySelector("body > div > div.grid")
+
+gridElement.addEventListener("mousedown", () => {
+    mouseDown = true
+})
+
+gridElement.addEventListener("mouseup", () => {
+    mouseDown = false
+})
+
+gridElement.addEventListener("mouseover", (event) => {
+    if (mouseDown) { // draw only when mouse is down
+        event.target.style.backgroundColor = "#707070";
+    }
+})
+
+// event delegation from buttons to board-buttons and decide what was the button from its innerText
+const densityElement = document.querySelector("body > div > div.board-buttons")
+densityElement.addEventListener("click", (event) => {
+    buildGridByDensity(event.target.innerText)
+});
+
+function buildGridByDensity(density) {
+    switch (density) {
+        case "small":
+            buildGrid(16, 22)
+            lastDensity = density
+            break
+        case "medium":
+            buildGrid(32, 44)
+            lastDensity = density
+            break
+        case "big":
+            buildGrid(64, 88)
+            lastDensity = density
+            break
+        case "erase":
+            erase()
+            break
+        default:
+        // do nothing
+    }
+}
+
+function buildGrid(rows, columns) {
+    removeAll()
+    const grid = document.querySelector("body > div > div.grid")
+
+    for (let i = 0; i < rows; i++) {
+        appendRow(grid, columns)
+    }
+}
+
+function appendRow(grid, columns) {
+    const row = document.createElement("div")
+    row.classList.add("row")
+
+    for (let i = 0; i < columns; i++) {
+        appendSquare(row)
+    }
+
+    grid.appendChild(row)
+}
+
+function appendSquare(row) {
+    const square = document.createElement("div")
+    square.classList.add("square")
+
+    row.appendChild(square)
+}
+
+function erase() {
+    removeAll()
+    buildGridByDensity(lastDensity)
+}
+
+function removeAll() {
+    document
+        .querySelectorAll('.row')
+        .forEach(row => { row.remove() })
+}
